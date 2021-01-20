cf16658448b105b2ae72ce421e4b0a7526c00ec7
Anton Malofeev <arenukvern@gmail.com>
2021-01-20 18:34:47 +0300

    Add: possibilty to build for production with ts support Bug: build doesn't includes tailwind

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from "./abstract/index";
 export * from "./components/index";
+// thanks to How to build:
+// https://forum.vuejs.org/t/vue-cli-library-build-error/86075/4
+// https://itnext.io/create-a-vue-js-component-library-as-a-module-part-1-a1116e632751
+import "./tailwind.css";
