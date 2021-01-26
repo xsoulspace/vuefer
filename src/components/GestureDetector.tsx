9a47b664aed183d2b90023ff8a239bb84abc7be0
Anton Malofeev <arenukvern@gmail.com>
2021-01-26 11:26:09 +0300

    fix:  getCurrentTracking in component setup

--- src/components/GestureDetector.tsx
+++ src/components/GestureDetector.tsx
@@ -9,12 +9,12 @@ export interface GestureDetectorI {
 export const GestureDetector = ({ child, onTap, key }: GestureDetectorI) => {
   return defineComponent({
     name: "GestureDetector",
+    setup() {
+      const onClick = onTap ? async () => await onTap() : undefined;
+      return { onClick };
+    },
     render() {
-      return h(
-        "div",
-        { onClick: onTap ? async () => await onTap() : undefined },
-        [h(child)]
-      );
+      return h("div", { onClick: this.onClick }, [h(child)]);
     },
   });
 };
