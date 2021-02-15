3610d5df619da6beec7aaef602042c9494eac672
Anton Malofeev <arenukvern@gmail.com>
2021-02-15 23:59:05 +0300

    chore: mocha to jest chore: all npm modules updates

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,12 @@
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    // const msg = 'new message'
-    // const wrapper = shallowMount(HelloWorld, {
-    //   props: { msg },
-    // })
-    // expect(wrapper.text()).to.include(msg)
-  })
-})
+// import { shallowMount } from "@vue/test-utils";
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+// describe("HelloWorld.vue", () => {
+//   it("renders props.msg when passed", () => {
+//     const msg = "new message";
+//     const wrapper = shallowMount(HelloWorld, {
+//       props: { msg }
+//     });
+//     expect(wrapper.text()).toMatch(msg);
+//   });
+// });
--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,13 @@
  * Discussed here https://github.com/vuejs/vue-cli/issues/1081
  */
 const fixEmitDeclarationFilesForTypeScript = {
-  chainWebpack: (config) => {
+  chainWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       config.module
         .rule('ts')
         .use('ts-loader')
         .loader('ts-loader')
-        .tap((options) => ({
+        .tap(options => ({
           ...options,
           transpileOnly: false,
           happyPackMode: false,
