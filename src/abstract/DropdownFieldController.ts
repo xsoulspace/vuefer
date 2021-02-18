b76c7585f7786e35b3ae6e282a04f916e64026da
Anton Malofeev <arenukvern@gmail.com>
2021-02-18 18:48:50 +0300

    feat: mutlidropdown controller wip: mutlidropdown button

--- src/abstract/DropdownFieldController.ts
+++ src/abstract/DropdownFieldController.ts
@@ -1,22 +1,45 @@
 import { reactive, Ref, ref } from 'vue'
 import { Maybe } from './BasicTypes'
 
-interface DropdownFieldControllerI<I> {
-  value?: Maybe<I>
+interface DropdownFieldControllerAbstractI {
   readOnly?: Maybe<boolean>
   maxLength?: Maybe<number>
   maxLines?: Maybe<number>
   key?: Maybe<string>
 }
-//  TODO: add properties
-export class DropdownFieldController<I> {
-  private _reactVal: { val: Maybe<I> | Record<string, unknown> } = reactive({
-    val: {},
-  })
+
+interface DropdownFieldControllerI<I> extends DropdownFieldControllerAbstractI {
+  value?: Maybe<I>
+}
+
+class DropdownFieldControllerAbstract {
   key: Ref<Maybe<string>> = ref()
   readOnly: boolean
   maxLength?: Maybe<number>
   maxLines?: Maybe<number>
+  constructor({
+    key,
+    maxLength,
+    maxLines,
+    readOnly,
+  }: DropdownFieldControllerAbstractI) {
+    this.key.value = key
+    this.readOnly = readOnly ?? false
+    this.maxLength = maxLength
+    this.maxLines = maxLines ?? 1
+  }
+  get css(): string {
+    return ''
+  }
+}
+
+//  TODO: add properties
+export class DropdownFieldController<
+  I
+> extends DropdownFieldControllerAbstract {
+  private _reactVal: { val: Maybe<I> | Record<string, unknown> } = reactive({
+    val: {},
+  })
   constructor({
     value,
     readOnly,
@@ -24,11 +47,13 @@ export class DropdownFieldController<I> {
     maxLength,
     key,
   }: DropdownFieldControllerI<I>) {
+    super({
+      key,
+      readOnly,
+      maxLines,
+      maxLength,
+    })
     this.value = value
-    this.key.value = key
-    this.readOnly = readOnly ?? false
-    this.maxLength = maxLength
-    this.maxLines = maxLines ?? 1
   }
   set value(val: Maybe<I>) {
     this._reactVal.val = val
@@ -37,8 +62,43 @@ export class DropdownFieldController<I> {
     return this._reactVal.val as Maybe<I>
   }
 
-  get css(): string {
-    return ''
+  get reactive() {
+    return this._reactVal
+  }
+}
+
+interface MutliDropdownFieldControllerI<I>
+  extends DropdownFieldControllerAbstractI {
+  value?: Maybe<I>[]
+}
+
+//  TODO: add properties
+export class MultiDropdownFieldController<
+  I
+> extends DropdownFieldControllerAbstract {
+  private _reactVal: { val: Maybe<I>[] } = reactive({
+    val: [],
+  })
+  constructor({
+    value,
+    readOnly,
+    maxLines,
+    maxLength,
+    key,
+  }: MutliDropdownFieldControllerI<I>) {
+    super({
+      key,
+      maxLength,
+      maxLines,
+      readOnly,
+    })
+    this.value = value ?? []
+  }
+  set value(val: Maybe<I>[]) {
+    this._reactVal.val = val
+  }
+  get value() {
+    return this._reactVal.val
   }
   get reactive() {
     return this._reactVal
---
+++ src/components/MutliDropdownButton.tsx
@@ -0,0 +1,171 @@
+import { Component, computed, defineComponent, h, ref, Ref } from 'vue'
+import { Maybe, ValueChanged } from '../abstract/BasicTypes'
+import { BoxDecoration } from '../abstract/BoxDecoration'
+import { BoxShadow } from '../abstract/BoxShadow'
+import { Colors } from '../abstract/Colors'
+import { DropdownFieldController } from '../abstract/DropdownFieldController'
+import { EdgeInsets, EdgeInsetsStep } from '../abstract/EdgeInsets'
+import { ItemBuilder } from '../abstract/ItemBuilder'
+import { MainAxisAlignment } from '../abstract/MainAxisAlignment'
+import { MainAxisSize } from '../abstract/MainAxisSize'
+import { TextEditingController } from '../abstract/TextEditingController'
+import clickOutside from '../directives/VClickOutside'
+import { unifyValue } from '../functions'
+import { Container } from './Container'
+import { DropdownMenuItemConstructor } from './DropdownMenuItem'
+import { GestureDetector } from './GestureDetector'
+import { Icon, Icons } from './Icon'
+import ListViewBuilder from './ListViewBuilder.vue'
+import { Positioned } from './Positioned'
+import { Row } from './Row'
+import { SizedBox } from './SizedBox'
+import { Stack } from './Stack'
+import { TextField } from './TextField'
+import { Visibility } from './Visibility'
+
+interface MultiDropdownButtonI<I> {
+  items: DropdownMenuItemConstructor<I>[]
+  minItemHeight?: Maybe<Ref<number>>
+  onChanged?: Maybe<ValueChanged<I>>
+  elevation?: Maybe<BoxShadow>
+  icon?: Maybe<Component>
+  controller: DropdownFieldController<I>
+}
+
+export const MultiDropdownButton = <
+  I extends
+    | string
+    | number
+    | boolean
+    | { [prop: string]: any }
+    | { [prop: number]: any }
+>({
+  items,
+  elevation,
+  icon,
+  minItemHeight,
+  controller,
+  onChanged,
+}: MultiDropdownButtonI<I>) => {
+  const resolvedIcon =
+    icon ?? Icon(Icons.arrow_drop_down, { size: EdgeInsetsStep.s12 })
+  const textFieldController = new TextEditingController({})
+  const selectedItem = computed(() =>
+    items.find((el) => el.key == controller.key.value)
+  )
+  // init
+  textFieldController.text.value = selectedItem.value?.title ?? ''
+  const isMenuOpened = ref(false)
+
+  const itemsDropdown = defineComponent({
+    name: 'items',
+    components: {
+      ListViewBuilder,
+    },
+    setup() {
+      const effectiveItems = computed(() =>
+        items.filter((el) => {
+          const isFound = unifyValue({ str: el?.value }).includes(
+            unifyValue({ str: textFieldController.text.value })
+          )
+          return isFound
+        })
+      )
+
+      const itemBuilder: ItemBuilder = ({ index }) => {
+        const item = effectiveItems.value[index]
+        if (item) return item.widget
+        return Container({})
+      }
+      return () =>
+        h(
+          Positioned({
+            top: EdgeInsetsStep.s11,
+            child: SizedBox({
+              child: Container({
+                padding: EdgeInsets.only({
+                  left: EdgeInsetsStep.s2,
+                  top: EdgeInsetsStep.s2,
+                }),
+                decoration: new BoxDecoration({
+                  boxShadow: elevation ?? BoxShadow.md,
+                  color: Colors.white,
+                }),
+                child: h(
+                  <list-view-builder
+                    itemCount={effectiveItems.value.length}
+                    itemBuilder={itemBuilder}
+                    minItemHeight={minItemHeight}
+                    onItemClick={(index: number) => {
+                      const oldValue = controller.value
+                      const item = effectiveItems.value[index]
+                      if (item == null) return Container({})
+                      controller.value = item.value
+                      controller.key.value = item.key
+                      textFieldController.text.value = item.title
+                      isMenuOpened.value = false
+                      if (onChanged && item.value) {
+                        onChanged(item.value, oldValue)
+                      }
+                    }}
+                  />
+                ),
+              }),
+              width: EdgeInsetsStep.s44,
+              height: EdgeInsetsStep.s56,
+            }),
+          })
+        )
+    },
+  })
+
+  return defineComponent({
+    name: 'DropdownButton',
+    directives: {
+      clickOutside,
+    },
+    setup() {
+      const closeMenu = () => {
+        isMenuOpened.value = false
+      }
+      return () =>
+        h(
+          <div v-click-outside={closeMenu}>
+            {h(
+              Stack({
+                children: [
+                  GestureDetector({
+                    child: SizedBox({
+                      child: Container({
+                        decoration: new BoxDecoration({
+                          boxShadow: elevation,
+                        }),
+                        child: Row({
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextField({ controller: textFieldController }),
+                            resolvedIcon,
+                          ],
+                        }),
+                      }),
+                      height: EdgeInsetsStep.s10,
+                    }),
+                    onTap: () => {
+                      if (!isMenuOpened.value) {
+                        isMenuOpened.value = true
+                      }
+                    },
+                  }),
+                  Visibility({
+                    child: itemsDropdown,
+                    visible: isMenuOpened,
+                  }),
+                ],
+              })
+            )}
+          </div>
+        )
+    },
+  })
+}
