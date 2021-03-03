a6090ba8d09390f5825cf9103fc38a8a1f0505d0
Anton Malofeev <arenukvern@gmail.com>
2021-03-03 12:11:36 +0300

    chore: example dependencies

--- packages/example/vite-project/src/main.ts
+++ packages/example/vite-project/src/main.ts
@@ -1,19 +1,19 @@
-import { HeroesModel } from '#/models/HeroesModel'
-import { ScaffoldApp } from '#/pages/ScaffoldApp'
+import { HeroesModel } from "#/models/HeroesModel";
+import { ScaffoldApp } from "#/pages/ScaffoldApp";
 import {
   MultiProvider,
   Navigation,
   NavigationController,
-} from '@xsoulspace/vuefer'
-import '@xsoulspace/vuefer/style.css'
-import { createApp } from 'vue'
-import vueGridLayout from 'vue-grid-layout'
+} from "@xsoulspace/vuefer";
+import "@xsoulspace/vuefer/style.css";
+import { createApp } from "vue";
+// import vueGridLayout from 'vue-grid-layout'
 
 const app = MultiProvider.build({
   models: [NavigationController, HeroesModel],
   child: Navigation({
     child: ScaffoldApp,
   }),
-})
+});
 
-createApp(app).use(vueGridLayout).mount('#app')
+createApp(app).mount("#app");
